47cd7f9431a64c2bfee8e244ab216a4f93ff16a6
Charles Caleb <charlesofficial9@gmail.com>
2025-01-11 11:37:41 +0100

    update

--- src/features/diary/Entries.tsx
+++ src/features/diary/Entries.tsx
@@ -17,6 +17,7 @@ export default function Entries() {
   const [viewEvent, setViewEvent] = useState<EventType | null>(null);
 
   const [searchDate, setSearchDate] = useState<Date | null>(null);
+  const [searchQuery, setSearchQuery] = useState<string>("");
   const [filteredEvents, setFilteredEvents] = useState<EventType[]>([]);
 
   const { data, isLoading } = useEntries();
@@ -26,9 +27,11 @@ export default function Entries() {
       const filtered = data.data
         .filter(
           (order: EventType) =>
-            !searchDate ||
-            format(parseISO(order.createdAt), "yyyy-MM-dd") ===
-              format(searchDate, "yyyy-MM-dd")
+            (!searchDate ||
+              format(parseISO(order.createdAt), "yyyy-MM-dd") ===
+                format(searchDate, "yyyy-MM-dd")) &&
+            (order.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+              order.content.toLowerCase().includes(searchQuery.toLowerCase()))
         )
         .sort(
           (a: EventType, b: EventType) =>
@@ -36,7 +39,7 @@ export default function Entries() {
         );
       setFilteredEvents(filtered);
     }
-  }, [searchDate, data?.data]);
+  }, [searchDate, searchQuery, data?.data]);
 
   const handleCloseModal = () => {
     setIsShowForm(false);
@@ -68,9 +71,9 @@ export default function Entries() {
         </Modal>
       )}
 
-      <div className="flex flex-col  items-center justify-center gap-3">
+      <div className="flex flex-col items-center justify-center gap-3">
         <button
-          className="flex items-center text-sm gap-2 px-2 md:px-3 py-1 md:py-2  font-bold text-teal-500 bg-white border rounded-lg hover:bg-blue-100"
+          className="flex items-center text-sm gap-2 px-2 md:px-3 py-1 md:py-2 font-bold text-teal-500 bg-white border rounded-lg hover:bg-blue-100"
           onClick={() => setIsShowForm(true)}
         >
           Add Note <BiMessageAdd />
@@ -80,12 +83,11 @@ export default function Entries() {
             selected={searchDate}
             onChange={(date) => setSearchDate(date)}
             dateFormat="yyyy-MM-dd"
-            className="w-full py-1 px-3 text-center text-rose-500  border-none focus:outline-none rounded-md text-base" // Set font-size to at least 16px and remove border
+            className="w-full py-1 px-3 text-center text-rose-500 border-none focus:outline-none rounded-md text-base" // Set font-size to at least 16px and remove border
             placeholderText="Date (yyyy-mm-dd)"
           />
-
           <span
-            className="text-xl text-red-500 absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
+            className="text-lg text-red-500 absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
             onClick={() => setSearchDate(null)}
           >
             x
@@ -93,16 +95,36 @@ export default function Entries() {
         </div>
       </div>
 
-      <div className="flex flex-col w-full  items-center justify-center ">
-        <h2 className="text-xl md:text-2xl font-bold text-white">MY NOTES</h2>
+      <div className="flex flex-col w-full items-center justify-center">
+        <div className=" w-full max-w-[610px] lg:max-w-[910px]  flex flex-col md:flex-row items-center justify-between  p-2 md:p-4 -mb-2 md:-mb-4 ">
+          <h2 className="text-xl md:text-2xl font-bold text-white">MY NOTES</h2>
+
+          <div className="relative w-full max-w-[285px]">
+            <input
+              type="text"
+              placeholder="Search by title or content"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="w-full  p-1  text-base bg-rose-600 text-center text-white placeholder:text-white border-2 border-teal-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-500"
+            />
+
+            <span
+              className="text-ld text-white absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
+              onClick={() => setSearchDate(null)}
+            >
+              x
+            </span>
+          </div>
+        </div>
+
         {filteredEvents.length < 1 && (
-          <div>
+          <div className="mt-8 ">
             <p>No Events Available</p>
           </div>
         )}
         {filteredEvents.length > 0 && (
           <div
-            className={`grid w-full max-w-[610px] lg:max-w-[910px] gap-2 md:gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-2 md:p-4 `}
+            className={`grid w-full max-w-[610px] lg:max-w-[910px] gap-2 md:gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-2 md:p-4`}
           >
             {filteredEvents?.map((entry: EventType, index: number) => (
               <div
