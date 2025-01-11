ffd26669ca7c2c6504ab6cc381905a703ed4cb5a
Charles Caleb <charlesofficial9@gmail.com>
2025-01-11 09:49:24 +0100

    update

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -2,24 +2,26 @@ import { Outlet } from "react-router-dom";
 import { useLogout } from "../features/authenticaton/useLogout";
 import SpinnerMini from "./SpinnerMini";
 import { BiLogOut } from "react-icons/bi";
+
 export default function AppLayout() {
   const { logout, isPending } = useLogout();
 
   const handleLogout = async () => {
     logout();
   };
+
   return (
     <div className="flex flex-col h-screen bg-gradient-to-br from-rose-600 to-gray-950 font-roboto tracking-wide">
       <header className="flex items-center justify-between bg-white shadow-md border-b-2 border-teal-500 px-4 py-2 top-0 z-10">
-        <div className="container mr-auto">
+        <div className="container mx-auto">
           <h1 className="text-xl font-bold text-teal-600">My Diary App</h1>
         </div>
-        <div className="flex justify-center items-center h-10  w-[115px] m border ml-2 md:ml-0 shadow-lg hover:bg-gray-800 hover:text-gray-50 transition-colors duration-200">
+        <div className="flex justify-center items-center h-10 w-[115px] border ml-2 md:ml-0 shadow-lg hover:bg-gray-800 hover:text-gray-50 transition-colors duration-200">
           {isPending ? (
             <SpinnerMini />
           ) : (
             <button
-              className="w-full h-full text-sm flex justify-center items-center gap-1 p-1 md:p-2 rounded "
+              className="w-full h-full text-sm flex justify-center items-center gap-1 p-1 md:p-2 rounded"
               onClick={handleLogout}
             >
               <BiLogOut />
@@ -28,7 +30,7 @@ export default function AppLayout() {
           )}
         </div>
       </header>
-      <main className="h-full text-white overflow-y-scroll  ">
+      <main className="flex-grow text-white overflow-y-scroll">
         <Outlet />
       </main>
       <footer className="border-t-2 border-teal-500 bg-white text-center py-4 mt-auto">
