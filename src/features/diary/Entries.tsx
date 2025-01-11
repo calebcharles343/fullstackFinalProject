0f29a6714fd63c567c7193ab13a8edd7caa9021b
Charles Caleb <charlesofficial9@gmail.com>
2025-01-11 09:47:22 +0100

    update

--- src/features/diary/Entries.tsx
+++ src/features/diary/Entries.tsx
@@ -59,7 +59,7 @@ export default function Entries() {
   }
 
   return (
-    <div className="flex flex-col items-center w-full gap-3 md:4 p-4 md:p-2">
+    <div className="flex flex-col items-center w-full min-w-[300px] h-screen gap-3 p-4 md:p-2">
       {isShowForm && (
         <Modal onClose={handleCloseModal}>
           <div className="mt-4 bg-white shadow-lg rounded-lg">
@@ -67,48 +67,58 @@ export default function Entries() {
           </div>
         </Modal>
       )}
-      <button
-        className="flex items-center text-sm gap-2 px-2 md:px-3 py-1 md:py-2  font-bold text-teal-500 bg-white border rounded-lg hover:bg-blue-100"
-        onClick={() => setIsShowForm(true)}
-      >
-        Add Event <BiMessageAdd />
-      </button>
-      <div className="relative flex flex-col items-center w-full max-w-[200px] bg-white text-sm border border-gray-500 rounded-lg focus-within:border-2 focus-within:border-teal-500">
-        <ReactDatePicker
-          selected={searchDate}
-          onChange={(date) => setSearchDate(date)}
-          dateFormat="yyyy-MM-dd"
-          className="w-full py-1 px-3 text-center text-rose-500  border-none focus:outline-none rounded-md text-base" // Set font-size to at least 16px and remove border
-          placeholderText="Date (yyyy-mm-dd)"
-        />
 
-        <span
-          className="text-xl text-red-500 absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
-          onClick={() => setSearchDate(null)}
+      <div className="flex flex-col  items-center justify-center gap-3">
+        <button
+          className="flex items-center text-sm gap-2 px-2 md:px-3 py-1 md:py-2  font-bold text-teal-500 bg-white border rounded-lg hover:bg-blue-100"
+          onClick={() => setIsShowForm(true)}
         >
-          x
-        </span>
+          Add Event <BiMessageAdd />
+        </button>
+        <div className="relative flex flex-col items-center w-full max-w-[200px] bg-white text-sm border border-gray-500 rounded-lg focus-within:border-2 focus-within:border-teal-500">
+          <ReactDatePicker
+            selected={searchDate}
+            onChange={(date) => setSearchDate(date)}
+            dateFormat="yyyy-MM-dd"
+            className="w-full py-1 px-3 text-center text-rose-500  border-none focus:outline-none rounded-md text-base" // Set font-size to at least 16px and remove border
+            placeholderText="Date (yyyy-mm-dd)"
+          />
+
+          <span
+            className="text-xl text-red-500 absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
+            onClick={() => setSearchDate(null)}
+          >
+            x
+          </span>
+        </div>
       </div>
 
-      <h2 className="text-xl  md:text-2xl font-bold text-white">MY EVENTS</h2>
-      {filteredEvents.length < 1 && <p>No Events Available</p>}
-      {filteredEvents.length > 0 && (
-        <div
-          className={`grid w-full max-w-[610px] lg:max-w-[910px] gap-2 md:gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-2 md:p-4 `}
-        >
-          {filteredEvents?.map((entry: EventType, index: number) => (
-            <div
-              key={index}
-              onClick={() => {
-                setEventId(entry.id);
-                setIsShowEvent(true);
-              }}
-            >
-              <Entry entry={entry} />
-            </div>
-          ))}
-        </div>
-      )}
+      <div className="flex flex-col w-full  items-center justify-center ">
+        <h2 className="text-xl md:text-2xl font-bold text-white">MY EVENTS</h2>
+        {filteredEvents.length < 1 && (
+          <div>
+            <p>No Events Available</p>
+          </div>
+        )}
+        {filteredEvents.length > 0 && (
+          <div
+            className={`grid w-full max-w-[610px] lg:max-w-[910px] gap-2 md:gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-2 md:p-4 `}
+          >
+            {filteredEvents?.map((entry: EventType, index: number) => (
+              <div
+                key={index}
+                onClick={() => {
+                  setEventId(entry.id);
+                  setIsShowEvent(true);
+                }}
+              >
+                <Entry entry={entry} />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
       {isShowEvent && viewEvent && (
         <Modal onClose={handleCloseModal}>
           <div className="w-full max-w-[400px] mt-4 bg-white shadow-lg rounded-lg">
